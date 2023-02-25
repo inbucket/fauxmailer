c4c9ce578bc5555321283506491f599bc9896f60
James Hillyerd <james@hillyerd.com>
2023-02-25 13:32:17 -0800

    nix/direnv setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go_1_20
+              golint
+              gopls
+            ];
+          };
+        });
+}
